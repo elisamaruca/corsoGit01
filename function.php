53f152b376b36a21b134aa2982cea00ef1cf49fe
Elisa Maruca <elisa97.em@gmail.com>
2022-08-19 21:44:10 +0200

    aggiunta divisione con controllo

--- function.php
+++ function.php
@@ -9,5 +9,14 @@ function sottrazione (n1,n2){
 function moltiplicazione (n1,n2){
     return n1*n2;}
 }
+function divisione (n1,n2){
+    if(n2 !=0)
+    return n1/n2;}
+    else{
+        return "error";
+    }
+
+   ;}
+}
 
 >
